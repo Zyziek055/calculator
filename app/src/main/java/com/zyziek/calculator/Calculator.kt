07529c1b35a43798d0fd7a5c842e6a7fb6a3df8b
zyziekpc <sylgermroz@gmail.com>
2025-07-17 14:11:48 +0200

    Added buttons with colors

--- app/src/main/java/com/zyziek/calculator/Calculator.kt
+++ app/src/main/java/com/zyziek/calculator/Calculator.kt
@@ -1,17 +1,24 @@
 package com.zyziek.calculator
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.TextStyle
@@ -20,11 +27,11 @@ import androidx.compose.ui.unit.dp
 
 
 val calculatorButtons = listOf(
-    "C", "←", "%", "÷",
+    "C", "±", "%", "÷",
     "7", "8", "9", "×",
     "4", "5", "6", "-",
     "1", "2", "3", "+",
-    "0", ".", "=", ""
+    "K", "0", ",", "="
 )
 
 /*
@@ -64,11 +71,37 @@ fun Calculator(modifier: Modifier = Modifier) {
             ) {
 
                 items(calculatorButtons) {
-                    Text(text = it)
+                    CalculatorButton(it)
                 }
 
             }
 
         }
     }
+}
+
+@Composable
+fun CalculatorButton(btn: String) {
+    Box(modifier = Modifier.padding(8.dp)) {
+        FloatingActionButton(
+            modifier = Modifier.size(80.dp),
+            onClick = {/*todo*/},
+            contentColor = Color.White,
+            containerColor = getColor(btn)
+        ) {
+            Text(text = btn)
+        }
+    }
+}
+
+fun getColor(btn: String): Color {
+    if (btn in listOf("C", "±", "%")) {
+        return Color(0xFFD4D4D2)
+    }
+
+    if(btn in listOf("÷","×", "-", "+",  "=")) {
+        return Color(0xFFFF9500)
+    }
+
+    return Color(0xFF505050)
 }
